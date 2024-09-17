683ed11c8d862cffbe3cdad4415a8500ad01dd3a
Victor Biga <victor.biga@gmail.com>
2024-09-17 16:42:23 +0200

    brew

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -70,6 +71,30 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.victor = import ./home.nix;
+        }
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "victor";
+
+            # Optional: Declarative tap management
+            #taps = {
+            #  "homebrew/homebrew-core" = homebrew-core;
+            #  "homebrew/homebrew-cask" = homebrew-cask;
+            #};
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            # mutableTaps = false;
+          };
         }
       ];
     };
